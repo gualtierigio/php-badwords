0ee27703459767e6abb74175463623d8bbbca5bf
Giovanni Gualtieri <gualtierigio@gmail.com>
2021-10-12 16:56:12 +0200

    inserisco metodo get

--- index.php
+++ index.php
@@ -1,18 +1,19 @@
-<?php
-    $testo = ['Creare una variabile con un paragrafo di testo a vostra scelta (possibilmente corposo).
-    Stampare a schermo il paragrafo e la sua lunghezza.
-    Una parola da censurare viene passata dall\'utente tramite parametro GET.'];
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>php-badwords</title>
 </head>
 <body>
-    <h1></h1>
-    <p><?php echo $testo ?></p>
+<?php
+    $title = 'Welcome php';
+    $testo = 'Creare una variabile con un paragrafo di testo a vostra scelta (possibilmente corposo). Stampare a schermo il paragrafo e la sua lunghezza. Una parola da censurare viene passata dall\'utente tramite parametro GET.';
+    $badWord = '***';
+?>
+    <h1><?php $title ?></h1>
+    <p><?php echo $testo . " lughezza= " . strlen($testo) ?></p>
+    <h2> <? echo $_GET['badWord'] ?></h2>
 </body>
 </html>
