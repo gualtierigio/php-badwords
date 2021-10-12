0d0ef5da1956a868bf3096f882d90b472373c485
Giovanni Gualtieri <gualtierigio@gmail.com>
2021-10-12 15:45:52 +0200

    php initialized

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+    $testo = ['Creare una variabile con un paragrafo di testo a vostra scelta (possibilmente corposo).
+    Stampare a schermo il paragrafo e la sua lunghezza.
+    Una parola da censurare viene passata dall\'utente tramite parametro GET.'];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1></h1>
+    <p><?php echo $testo ?></p>
+</body>
+</html>
